4fd3b5b59bda4cb6028094eac7417b2866ff796a
Martin Maly <martinmaly66@hotmail.com>
2023-04-19 23:33:26 +0900

    Things working a bit better

--- Martin Maly - Swift Student Challenge Submission.swiftpm/View/BreathPlayGameView.swift
+++ Martin Maly - Swift Student Challenge Submission.swiftpm/View/BreathPlayGameView.swift
@@ -4,28 +4,40 @@ import SceneKit
 struct BreathPlayGameView: UIViewRepresentable {
     public let sceneView = SCNView()
     public let breathBoxNode = SCNNode()
+    public let focusNode = SCNNode()
     public let selfieStickNode = SCNNode()
     
     class Coordinator: NSObject, SCNSceneRendererDelegate, UIGestureRecognizerDelegate {
         let parent: BreathPlayGameView
+        var timer = Timer()
         
         init(_ parent: BreathPlayGameView) {
             self.parent = parent
             super.init()
+            
+            self.timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { _ in
+                let offset: Float = 30
+                
+                if let breathingType = MicrophoneManager.shared.breathingType {
+                    switch breathingType {
+                    case .inhale: 
+                        parent.breathBoxNode.position.x = offset
+                    case .exhale: 
+                        parent.breathBoxNode.position.x = -offset
+                    }
+                }
+                
+                parent.breathBoxNode.position.z -= 2
+                parent.focusNode.position.z -= 2
+            })
         }
         
         func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-            if let breathingType = MicrophoneManager.shared.breathingType {
-                switch breathingType {
-                case .inhale: 
-                    parent.breathBoxNode.physicsBody?.applyForce(SCNVector3(x: 0, y:0, z: 0), asImpulse: true)
-                    
-                case .exhale: 
-                    parent.breathBoxNode.physicsBody?.applyForce(SCNVector3(x: 0, y:0, z: -1), asImpulse: true)
-                }
-            }
+            //
         }
     }
+    
+    
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
@@ -36,30 +48,22 @@ struct BreathPlayGameView: UIViewRepresentable {
         
         let scene = SCNScene()
         
-        let floorGeometry = SCNFloor()
-        let floorNode = SCNNode(geometry: floorGeometry)
-        floorNode.physicsBody = SCNPhysicsBody(
-            type: .static, 
-            shape: .init(geometry: SCNFloor(), options: nil)
-        )
-        floorNode.physicsBody?.categoryBitMask = Constants.BitMask.floor.rawValue
-        floorNode.physicsBody?.collisionBitMask = Constants.BitMask.floor.rawValue
-        floorNode.geometry?.materials.first?.diffuse.contents = UIImage.init(named: "floor.jpeg")
-        scene.rootNode.addChildNode(floorNode)
+        let focusNodeGeometry = SCNSphere(radius: 0.001)
+        focusNode.geometry = focusNodeGeometry
+        focusNode.worldPosition = SCNVector3(x: 0, y: 5, z: 0)
+        scene.rootNode.addChildNode(focusNode)
         
-        let boxGeometry = SCNBox(width: 3, height: 3, length: 3, chamferRadius: 0.5)
+        let boxGeometry = SCNBox(width: 10, height: 10, length: 10, chamferRadius: 1)
         breathBoxNode.geometry = boxGeometry
-        breathBoxNode.position = SCNVector3(x: 0, y: 1.5, z: 1.5)
-        breathBoxNode.physicsBody = SCNPhysicsBody(
-            type: .dynamic, 
-            shape: .init(node: breathBoxNode, options: nil)
-        )
+        breathBoxNode.worldPosition = SCNVector3(x: 0, y: 5, z: 0)
         breathBoxNode.physicsBody?.collisionBitMask = Constants.BitMask.floor.rawValue
-        floorNode.addChildNode(breathBoxNode)
+        scene.rootNode.addChildNode(breathBoxNode)
         
         selfieStickNode.camera = SCNCamera()
+        selfieStickNode.camera?.zFar = 1000
         selfieStickNode.position = SCNVector3(0, 8, 5)
         scene.rootNode.addChildNode(selfieStickNode)
+        
         
         let lightNode = SCNNode()
         lightNode.light = SCNLight()
@@ -67,13 +71,13 @@ struct BreathPlayGameView: UIViewRepresentable {
         lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
         scene.rootNode.addChildNode(lightNode)
         
-        let cameraZoom = 40
-        let cameraZPos = cameraZoom / 2 // cameraZoom == 100
-        let replicatorConstraint = SCNReplicatorConstraint(target: breathBoxNode)
-        replicatorConstraint.positionOffset = SCNVector3(0 ,cameraZoom, cameraZPos)
+        let cameraZoom = 50
+        let cameraZPos = cameraZoom / 1 // cameraZoom == 100
+        let replicatorConstraint = SCNReplicatorConstraint(target: focusNode)
+        replicatorConstraint.positionOffset = SCNVector3(0 ,60, 80)
         replicatorConstraint.replicatesOrientation = false
         
-        let lookAtConstraint = SCNLookAtConstraint(target: breathBoxNode)
+        let lookAtConstraint = SCNLookAtConstraint(target: focusNode)
         lookAtConstraint.influenceFactor = 0.07
         lookAtConstraint.isGimbalLockEnabled = true
         
@@ -91,12 +95,12 @@ struct BreathPlayGameView: UIViewRepresentable {
             //add plane
             //negative z will be our 'forward' direction
             let planeWidth: CGFloat = 150
-            let planeHeight: CGFloat = 40
+            let planeHeight: CGFloat = 180
             let planeShape = SCNPlane(width: planeWidth, height: planeHeight)
             let planeShapeNode = SCNNode(geometry: planeShape)
             planeShapeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.random()
-            
-            planeShapeNode.position = SCNVector3.init(x: 0, y: 0.0001, z: -(Float(planeHeight) / 2) - (Float(i) * Float(planeHeight)))
+            planeShapeNode.geometry?.firstMaterial?.isDoubleSided = true
+            planeShapeNode.position = SCNVector3.init(x: 0, y: -0.0001, z: -(Float(planeHeight) / 2) - (Float(i) * Float(planeHeight)))
             planeShapeNode.eulerAngles = SCNVector3(CGFloat.pi * -0.5, 0.0, 0.0)
             scene.rootNode.addChildNode(planeShapeNode)
         }
